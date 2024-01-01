1b9ddf88a579e3be4f2b767f64f9061685d78c54
Vasily Subbotin <subv13@yandex.ru>
2024-01-01 15:50:14 +0500

    add autumnal paradise v1

--- pages/autumnal_paradise/index.js
+++ pages/autumnal_paradise/index.js
@@ -1,49 +1,168 @@
 import * as THREE from 'three'
 import vertexShader from './shader.vert?raw'
 import fragmentShader from './shader.frag?raw'
+import { action, createMachine, immediate, interpret, invoke, reduce, state, transition } from 'robot3'
+import { Pane } from 'tweakpane'
 
-const scene = new THREE.Scene()
-
-const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1)
-
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
-
-const uniforms = {
-    u_time: { value: 0 },
-    u_resolution: {
-        value: new THREE.Vector3(window.innerWidth, window.innerHeight, 1),
+const CONTEXT = () => ({
+    scene: undefined,
+    camera: undefined,
+    renderer: undefined,
+    uniforms: {
+        u_param: { value: 1.0 },
+        u_time: { value: 0.0 },
+        u_resolution: {
+            value: new THREE.Vector3(window.innerWidth, window.innerHeight, 1.0),
+        },
+        u_mouse: {
+            value: new THREE.Vector2(),
+        },
+        u_sound: {
+            value: new THREE.Vector2(),
+        },
     },
-}
-
-const plane = new THREE.PlaneGeometry(2, 2)
-const material = new THREE.ShaderMaterial({
-    vertexShader,
-    fragmentShader,
-    uniforms,
+    clock: undefined,
+    analyser: undefined,
+    uiState: {
+        param: 1.0,
+    },
 })
-scene.add(new THREE.Mesh(plane, material))
 
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera.updateProjectionMatrix()
-    uniforms.u_resolution.value.set(window.innerWidth, window.innerHeight, 1)
-    renderer.setSize(window.innerWidth, window.innerHeight)
+const FSM = createMachine(
+    {
+        IDLE: state(immediate('CREATING_UI')),
+        CREATING_UI: invoke(
+            createUI,
+            transition(
+                'done',
+                'CREATING_SCENE',
+                reduce((context, event) => ({ ...context })),
+            ),
+        ),
+        CREATING_SCENE: invoke(
+            createScene,
+            transition(
+                'done',
+                'CREATING_SOUND_ANALYSER',
+                reduce((context, { data: { scene, camera, renderer, clock } }) => ({
+                    ...context,
+                    scene,
+                    camera,
+                    renderer,
+                    clock,
+                })),
+            ),
+            transition('error', 'ERROR', action(console.log)),
+        ),
+        CREATING_SOUND_ANALYSER: invoke(
+            createSoundAnalyser,
+            transition(
+                'done',
+                'ANIMATING_SCENE',
+                reduce((context, { data: { analyser } }) => ({
+                    ...context,
+                    analyser,
+                })),
+            ),
+            transition('error', 'ERROR', action(console.log)),
+        ),
+        ANIMATING_SCENE: state(transition('PAUSE_SCENE', 'SCENE_PAUSED')),
+        SCENE_PAUSED: state(transition('ANIMATE_SCENE', 'ANIMATING_SCENE')),
+        ERROR: state(),
+    },
+    CONTEXT,
+)
+
+const SERVICE = interpret(FSM, () => {})
+
+async function createUI(context) {
+    const pane = new Pane({ title: 'Params' })
+
+    pane.addButton({
+        title: 'Pause scene',
+    }).on('click', event => {
+        if (SERVICE.machine.state.name === 'ANIMATING_SCENE') {
+            SERVICE.send('PAUSE_SCENE')
+            event.target.title = 'Animate scene'
+        } else if (SERVICE.machine.state.name === 'SCENE_PAUSED') {
+            SERVICE.send('ANIMATE_SCENE')
+            event.target.title = 'Pause scene'
+        }
+    })
+
+    pane.addBinding(context.uiState, 'param', {
+        label: 'Param',
+    }).on('change', event => {
+        context.uniforms.u_param.value = event.value
+    })
 }
 
-const clock = new THREE.Clock()
+async function createScene(context) {
+    const scene = new THREE.Scene()
+    const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1)
+    const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('canvas') })
+    const plane = new THREE.PlaneGeometry(2, 2)
+    const material = new THREE.ShaderMaterial({
+        vertexShader,
+        fragmentShader,
+        uniforms: context.uniforms,
+    })
+    scene.add(new THREE.Mesh(plane, material))
 
-function animate() {
-    requestAnimationFrame(animate)
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    window.addEventListener(
+        'resize',
+        () => {
+            camera.updateProjectionMatrix()
+            context.uniforms.u_resolution.value.set(window.innerWidth, window.innerHeight, 1)
+            renderer.setSize(window.innerWidth, window.innerHeight)
+        },
+        false,
+    )
 
-    uniforms.u_time.value = clock.getElapsedTime()
+    const clock = new THREE.Clock()
 
-    render()
+    return {
+        scene,
+        camera,
+        renderer,
+        clock,
+    }
+}
+
+async function createSoundAnalyser(context) {
+    let listener = context.camera.getObjectByName('listener')
+    if (listener !== undefined) {
+        context.camera.remove(listener)
+    }
+    listener = new THREE.AudioListener()
+    const sound = new THREE.Audio(listener)
+
+    navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(handleSuccess)
+
+    function handleSuccess(stream) {
+        let source = listener.context.createMediaStreamSource(stream)
+        sound.setNodeSource(source)
+    }
+
+    const analyser = new THREE.AudioAnalyser(sound, 512)
+
+    context.camera.add(listener)
+
+    return { analyser }
 }
 
 function render() {
+    requestAnimationFrame(render)
+
+    if (SERVICE.machine.state.name !== 'ANIMATING_SCENE') return
+
+    const { scene, camera, renderer, uniforms, clock, analyser } = SERVICE.context
+    uniforms.u_time.value = clock.getElapsedTime()
+
+    const data = analyser.getFrequencyData()
+    uniforms.u_sound.value.set((data[0] + data[1] + data[3]) / 255 / 3, (data[110] + data[111] + data[112]) / 255 / 3)
+
     renderer.render(scene, camera)
 }
-
-animate()
+render()
--- pages/autumnal_paradise/shader.frag
+++ pages/autumnal_paradise/shader.frag
@@ -1,14 +1,50 @@
 uniform vec3 u_resolution;
 uniform float u_time;
+uniform vec2 u_sound;
 
-float circle(vec2 uv, vec2 pos, float r) {
-    float d = length(uv - pos);
-    return smoothstep(r, r - 0.001, d);
+mat2 rot2D(float angle) {
+    float s = sin(angle);
+    float c = cos(angle);
+
+    return mat2(c, -s, s, c);
 }
 
-float rand(float v){
-    return (fract(sin(v)*100000.0) - 0.5) * 2.0;
+float smin(float a, float b, float k) {
+    float h = clamp(0.5 + 0.5 * (a-b) / k, 0.0, 1.0);
+    return mix(a, b, h) - k * h *(1.0-h);
 }
+
+float sdSphere(vec3 p, float s) {
+    return length(p) - s;
+}
+
+float sdBox(vec3 p, vec3 b)  {
+    vec3 q = abs(p) - b;
+    return length(max(q, 0.0)) + min(max(q.x, max(q.y, q.z)), 0.0);
+}
+
+float map(vec3 p) {
+    //    vec3 pos = vec3(sin(u_time) * 3.0, 0.0, 0.0);
+    float sphere = sdSphere(p, 0.3);
+
+    vec3 q = p;
+    vec2 k = vec2(0.0, abs(sin(u_time * .5 - floor(p) + trunc(p))));
+    k = vec2(0.0);
+    q.xy = mod(q.xy + k, 0.4) - 0.2;
+    q.xy *= rot2D(2.0 * u_time);
+    float box = sdBox(q, vec3(0.1));
+
+    return smin(box, sphere, 0.1);
+}
+
+vec3 palete(float t, vec3 a, vec3 b, vec3 c, vec3 d) {
+    return a + b * cos(6.28318 * (c * t + d));
+}
+
+vec3 palete1(float t) {
+    return palete(t, vec3(0.8, 0.5, 0.4), vec3(0.2, 0.4, 0.2), vec3(2.0, 1.0, 1.0), vec3(0.0, 0.25, 0.25));
+}
+
 
 void main() {
     // move origin to center
@@ -16,19 +52,29 @@ void main() {
     // apply aspect ratio
     uv *= vec2(u_resolution.x / u_resolution.y, 1);
 
-    vec3 finalColor = vec3(1.0);
+    vec3 color = vec3(0.0);
 
-//    for (int i = 0; i < 10; i++) {
-//        float fi = float(i);
-//        vec2 dir = vec2(0.0, -mod(-0.5 + u_time * 0.4 + u_time * 0.3 * abs(floor(fi * rand(10.0 * fi) - 1.33)), 2.0));
-//        vec2 pos = vec2(0.0 + sin(float(i)), 1.0) + dir;
-//        float d = circle(uv, pos, 0.03);
-//        vec3 color = vec3(1.0) * d;
-//
-//        finalColor *= color;
-//    }
+    vec3 ro = vec3(0, 0, -3);
+    vec3 rd = normalize(vec3(uv, 2.5));
 
-    finalColor = vec3(0.0) * circle(uv, vec2(0.0), 0.3);
+    float td = 0.0;
 
-    gl_FragColor = vec4(finalColor, 1.0);
+    int i;
+    for (i = 0; i < 80; i++) {
+        vec3 p = ro + rd * td;
+        p.xy *= 1.0 / cos(u_sound[0]);
+
+        p.z += sin(td);
+
+        float d = map(p);
+
+        td += d;
+
+        if (d < 0.001) break;
+        if (td > 100.0) break;
+    }
+
+    color = palete1(td * 0.2 + u_time * .2);
+
+    gl_FragColor = vec4(color, 1.0);
 }
